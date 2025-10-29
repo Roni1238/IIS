5a868b587d2fc76bf0438a115a6e8677f27e27ff
Roni1238 <veritir@hotmail.com>
2025-10-29 11:43:34 -0600

    se subio el archivo

---
+++ tareas/byte.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+printf("=== TAMAÑO DE DIFERENTES TIPOS DE PUNTEROS ===\n\n");
+// tipos de datos básicos
+printf("int = %zu bytes\n", sizeof(int));
+printf("char = %zu bytes\n", sizeof(char*));
+printf("float = %zu bytes\n", sizeof(float));
+printf("double = %zu bytes\n", sizeof(double));
+// Punteros básicos
+printf("int* = %zu bytes\n", sizeof(int*));
+printf("char* = %zu bytes\n", sizeof(char*));
+printf("float* = %zu bytes\n", sizeof(float*));
+printf("double* = %zu bytes\n", sizeof(double*));
+}
